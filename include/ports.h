faf8318d9ac18079770bd610e97e2d07dcb6c2e5
Luke Monroe Cranfill <lcranfil@ecelnx53.ecn.purdue.edu>
2025-04-07 19:18:41 -0400

    Added sd card basic intialization along with modifiying setup of SPI

---
+++ include/ports.h
@@ -0,0 +1,18 @@
+// #ifndef _PORTS_H_
+// #define _PORTS_H_
+
+//     // 12 NSS
+//     // 13 sck
+//     // 14 miso
+//     // 15 mosi
+
+
+// // port definitions below are used for easy reference
+// // they hold the address of the port
+// #define NSS_PORT () // 
+// #define SCK_PORT ()
+// #define MISO_PORT ()
+// #define MOSI_PORT ()
+
+
+// #endif
