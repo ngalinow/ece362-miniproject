80d94191f6d8a456ecf5beb2c1ae69f4c7bc064e
Shivoy Sharma <sharm640@eceprog5.ecn.purdue.edu>
2025-04-07 15:21:43 -0400

    added gameflow files

---
+++ include/gameflow.h
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+
+#define board_size 10
+#define num_ships 3 
+
+// number of ships can change
+
