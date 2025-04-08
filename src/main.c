1a1b99b80352b40f5d53b616ba969300c0e154e4
Nathan James Galinowski <ngalinow@ecelnx52.ecn.purdue.edu>
2025-04-08 10:03:56 -0400

    Moved tft display code into its own .c and .h files.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,11 @@
-#include <stdlib.h>
 #include "stm32f0xx.h"
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "tty.h"
-#include "fifo.h"
-#include "commands.h"
 #include "lcd.h"
+#include "tft_display.h"
 
-
-#define FIFOSIZE 16
-char serfifo[FIFOSIZE];
-int seroffset = 0;
 
 extern void internal_clock();
 
@@ -58,107 +53,6 @@ void init_sd_dma_RX() {
     DMA1_Channel4 -> CCR &= ~(0x1);
 }
 
-void init_spi1_slow(void) {    
-    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-   
-    GPIOB->MODER &= ~((0b11 << (3 * 2)) | (0b11 << (4 * 2)) | (0b11 << (5 * 2)));
-    GPIOB->MODER |= ((0b10 << (3 * 2)) | (0b10 << (4 * 2)) | (0b10 << (5 * 2)));;
-    GPIOB->AFR[0] &= ~(GPIO_AFRL_AFSEL3 | GPIO_AFRL_AFSEL4 | GPIO_AFRL_AFSEL5);
-   
-    SPI1->CR1 &= ~SPI_CR1_BR;
-    SPI1->CR1 |= SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0;
-    SPI1->CR1 |= SPI_CR1_MSTR;
-    SPI1->CR2 |= SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2;
-    SPI1->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI; 
-    SPI1->CR2 |= SPI_CR2_FRXTH;
-    SPI1->CR1 |= SPI_CR1_SPE;
- }
- 
- 
- void sdcard_io_high_speed() {
-    SPI1->CR1 &= ~SPI_CR1_SPE;
-    SPI1->CR1 &= ~SPI_CR1_BR;
-    SPI1->CR1 |= (SPI_CR1_BR_0);
-    SPI1->CR1 |= SPI_CR1_SPE;
- }
- 
- 
- void init_lcd_spi() {
-    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-    GPIOB -> MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER11_0 | GPIO_MODER_MODER14_0;
-    init_spi1_slow();
-    sdcard_io_high_speed();
- }
-
-int __io_putchar(int c) {
-    if (c == '\n') {
-        while (!(USART5->ISR & USART_ISR_TXE));
-        USART5->TDR = '\r';
-    }
-
-    while(!(USART5->ISR & USART_ISR_TXE));
-    USART5->TDR = c;
-    return c;
-}
-
-void draw_grid() {
-    LCD_Setup();
-    LCD_DrawFillRectangle(0, 0, 240, 320, 0x0f0f);
-
-    for(int x = 24; x < 240; x += 24){
-        LCD_DrawLine(x, 0, x, 320, 2);
-    }
-
-    for(int y = 32; y < 320; y += 32){
-        LCD_DrawLine(0, y, 240, y, 2);
-    }
-}
-
-void setup() {
-    internal_clock();
-    setbuf(stdin,0);
-    setbuf(stdout,0);
-    setbuf(stderr,0);
-    init_lcd_spi();
-    draw_grid();
-}
-
- // NOTES FOR THE TFT DISPLAY:
-
- // drawfillrect 0 0 200 200 0f0f
-// 0 0 (x and y coord based on top-left corner near purple wire)
-// 200 200 (width and height of the rectangle) (240 x 320 is the TFT MAX)
-// 0f0f (color)
-
-
-// drawline 0 0 200 200 2
-// 0 0 (x and y coord start point based on top-left corner near purple wire)
-// 200 200 (end point of the line)
-// 2 (thickness of the line)
-
-
-// CODE FOR THE GRID
-// drawfillrect 0 0 240 320 0f0f
-// drawline 24 0 24 320 2
-// drawline 48 0 48 320 2
-// drawline 72 0 72 320 2
-// drawline 96 0 96 320 2
-// drawline 120 0 120 320 2
-// drawline 144 0 144 320 2
-// drawline 168 0 168 320 2
-// drawline 192 0 192 320 2
-// drawline 216 0 216 320 2
-// drawline 0 32 240 32 2
-// drawline 0 64 240 64 2
-// drawline 0 96 240 96 2
-// drawline 0 128 240 128 2
-// drawline 0 160 240 160 2
-// drawline 0 192 240 192 2
-// drawline 0 224 240 224 2
-// drawline 0 256 240 256 2
-// drawline 0 288 240 288 2
-
 int main() {
-    setup();
+    setup_grid();
 }
