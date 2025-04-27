332cea2364c94d76e7baee5187cd04d67203956b
Luke Monroe Cranfill <lcranfil@ecelnx53.ecn.purdue.edu>
2025-04-27 14:39:05 -0400

    Fixed up SD card implementation with multimaster

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 extern void internal_clock();
 extern int sd_card_init_sequance();
 extern void nano_wait(unsigned int n);
+extern int sd_card_init_sequance();
 
 char keymap[16] = {
     '1', '2', '3', 'A',
@@ -20,31 +21,6 @@ char keymap[16] = {
 
 uint8_t col = 0;
 
-void init_spi_sd() {
-    RCC -> APB1ENR |= RCC_APB1ENR_SPI2EN;
-    RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
-
-    // 12 NSS
-    // 13 sck
-    // 14 miso
-    // 15 mosi
-
-    GPIOB -> MODER |= 0xAA << 24; // sets pins 12-15 as alternate function
-    GPIOB -> MODER |= 0x1 << 4; // PB2 set to output for turning the SD card on / off
-    
-    SPI2 -> CR1 &= ~SPI_CR1_SPE;
-
-    SPI2 -> CR1 |= 0x7 << 3; // baud rate at slowest (sclk speed set to 187.5 KHz) 
-    SPI2 -> CR2 |= 0x7 << 8; // sets data to 8 bits (1 byte per transaction)
-
-    SPI2 -> CR1 |= SPI_CR1_MSTR; // master mode configuration
-    SPI2 -> CR2 |= SPI_CR2_SSOE; // SS output enabled, multimaster not allowed
-    SPI2 -> CR1 |= SPI_CR1_SSM; // enables software slave management (SSI bit determines NSS)
-    SPI2 -> CR1 |= SPI_CR1_SSI; // controls the nss for our controller
-    SPI2 -> CR2 |= SPI_CR2_FRXTH; // lets us know we have our byte ready to read
-    
-    SPI2 -> CR1 |= SPI_CR1_SPE;
-}
 
 int main() {
     internal_clock();
--- src/sd_card.c
+++ src/sd_card.c
@@ -7,20 +7,35 @@ extern void nano_wait(unsigned int n);
 int write_game_data(int data[10]);
 int read_data(int data[10]);
 
+void init_spi_sd() {
+    RCC -> APB1ENR |= RCC_APB1ENR_SPI2EN;
+    RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
+
+    // 12 NSS
+    // 13 sck
+    // 14 miso
+    // 15 mosi
+
+    GPIOB -> MODER |= 0xAA << 24; // sets pins 12-15 as alternate function
+    GPIOB -> MODER |= 0x1 << 4; // PB2 set to output for turning the SD card on / off
+    
+    SPI2 -> CR1 &= ~SPI_CR1_SPE;
+
+    SPI2 -> CR1 |= 0x7 << 3; // baud rate at slowest (sclk speed set to 187.5 KHz) 
+    SPI2 -> CR2 |= 0x7 << 8; // sets data to 8 bits (1 byte per transaction)
+
+    SPI2 -> CR1 |= SPI_CR1_MSTR; // master mode configuration
+    SPI2 -> CR2 |= SPI_CR2_FRXTH; // lets us know we have our byte ready to read
+    
+    SPI2 -> CR1 |= SPI_CR1_SPE;
+}
+
 void enable_sd_card() {
     GPIOB -> ODR &= ~GPIO_ODR_2;
 }
 
 void disable_sd_card() {
     GPIOB -> ODR |= GPIO_ODR_2;
-}
-
-void enable_stm() {
-    SPI2 -> CR1 &= ~SPI_CR1_SSI;
-}
-
-void disable_stm() {
-    SPI2 -> CR1 |= SPI_CR1_SSI;
 }
 
 void send_data(char b) {
