a559338f378998eefc5216db387e98c3a34a9549
Shivoy Sharma <sharm640@ecelnx52.ecn.purdue.edu>
2025-04-27 22:34:33 -0400

    start game

---
+++ src/start.c
@@ -0,0 +1,11 @@
+
+// start code for game. Using some gameflow functions. start game, save game state
+// add functions to update tft dispay,
+// Add functions to have the waiting in between turns
+// Add function that checks the 
+
+int main()
+
+{
+
+}
